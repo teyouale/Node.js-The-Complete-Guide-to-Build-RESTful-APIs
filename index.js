6647442e52b6aa0cb28e135300543408c5804b79
eyouale <51370727+eyouale@users.noreply.github.com>
2020-04-17 20:10:56 +0100

    Buliding Your First Web Server

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-var express = require ('express');
-var app = express()
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+
+app.get('/api/courses',(req,res)=>{
+    res.send([1,2,3]);
+});
+
+app.listen(3000,() => console.log('Listening On Port 3000...'));
