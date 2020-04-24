6f73024100027d381c246197bd09004c1f21071e
eyouale <51370727+eyouale@users.noreply.github.com>
2020-04-24 12:04:49 +0100

    Handling  HTTP Get Requests

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const courses =[
+    {id: 1, name:'course1'},
+    {id: 2, name:'course1'},
+    {id: 3, name:'course1'},
+];
+
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
 app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3]);
+    res.send(courses);
 });
 ///Routes
 app.get('/api/courses/:id',(req,res)=>{
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) // 404 Object Not Found
+        res.status(404).send('The course with the given ID not found');
+    res.send(course) 
 });
-app.get('/api/posts/:year/:month',(req,res)=>{
-    res.send(req.params);
-});
+
 /*  Query Parameter if we can make http://localhost:3000/api/posts/25/45%20?%20sortBy=name
     app.get('/api/posts/:year/:month',(req,res)=>{
          res.send(req.query);
