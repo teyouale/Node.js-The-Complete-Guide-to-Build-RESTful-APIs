4db66c23bd9a270e718b55d53fc929d943333730
eyouale <51370727+eyouale@users.noreply.github.com>
2020-05-17 19:00:36 +0300

    Installing Joi and Working With Joi, Input Validation

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-const express = require('express');
+const express = require("express");
+const Joi = require("joi");
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); // Enabling JSON Parasing
 
-const courses =[
-    {id: 1, name:'course1'},
-    {id: 2, name:'course2'},
-    {id: 3, name:'course3'},
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
 ];
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get('/api/courses',(req,res)=>{
-    res.send(courses);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 ///Routes
-app.get('/api/courses/:id',(req,res)=>{
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) // 404 Object Not Found
-        res.status(404).send('The course with the given ID not found');
-    res.send(course) 
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    // 404 Object Not Found
+    res.status(404).send("The course with the given ID not found");
+  res.send(course);
 });
 
-app.post('/api/courses',(req,res)=>{
-    const course ={
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course); // Pushing course object into courses
-    res.send(course);
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course); // Pushing course object into courses
+  res.send(course);
 });
 
 /*  Query Parameter if we can make http://localhost:3000/api/posts/25/45%20?%20sortBy=name
@@ -40,4 +52,4 @@ app.post('/api/courses',(req,res)=>{
     });
  */
 
-app.listen(port,() => console.log(`Listening On Port ${port}...`));
+app.listen(port, () => console.log(`Listening On Port ${port}...`));
