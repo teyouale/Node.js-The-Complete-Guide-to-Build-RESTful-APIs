8c5cb64f21ced95d5deb3698da62c5957db497a3
eyouale <51370727+eyouale@users.noreply.github.com>
2020-05-22 07:17:34 +0300

    Installing mongoose and Connecting Succesfully

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var express = require ('express');
-var app = express()
+const mongoose = require('mongoose');
+
+mongoose
+	.connect('mongodb://localhost:27017/courses', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('Connected to Mongodb'));
