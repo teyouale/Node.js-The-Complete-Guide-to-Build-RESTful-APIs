61eb912c66152260ff2eaf5838dd16ad2cc133fd
eyouale <51370727+eyouale@users.noreply.github.com>
2020-05-25 16:18:51 +0300

    Creating a Schema

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const mongoose = require('mongoose');
 mongoose
 	.connect('mongodb://localhost:27017/courses', {
 		useNewUrlParser: true,
-		useUnifiedTopology: true,
+		useUnifiedTopology: true
 	})
 	.then(() => console.log('Connected to Mongodb'));
+
+const courseSchema = new mongoose.Schema({
+	name: String,
+	author: String,
+	tags: [String],
+	date: { type: Date, default: Date.now },
+	isPublished: Boolean
+});
+async function createCourse() {
+	const Course = mongoose.model('Course', courseSchema); // Pascal Convation Course =  Class  course=object
+	const course = new Course({
+		name: 'Node.js Course',
+		author: 'Mosh',
+		tags: ['node', 'backend'],
+		isPublished: true
+	});
+	const result = await course.save();
+	console.log(result);
+}
+createCourse();
